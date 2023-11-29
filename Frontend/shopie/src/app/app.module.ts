9c8858a35fb3b8fa88676c67372f2bb4118e8b4c
Rushion18! <francisgchegge@gmail.com.com>
2023-11-29 10:51:21 +0300

    added components for the landing page

--- Frontend/shopie/src/app/app.module.ts
+++ Frontend/shopie/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroHeaderComponent } from './hero-header/hero-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { ProductSingleComponent } from './product-single/product-single.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroHeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    BlogComponent,
+    CartComponent,
+    CheckoutComponent,
+    ContactComponent,
+    ProductComponent,
+    ProductSingleComponent,
+    BlogSingleComponent
   ],
   imports: [
     BrowserModule,
